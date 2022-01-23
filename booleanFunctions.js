15755c21ca415977d7831450c8d642d2c77c67dd
bed-com-18-19 <bed-com-18-19@unima.ac.mw>
2022-01-22 20:37:31 -0800

    second comit

--- booleanFunctions.js
+++ booleanFunctions.js
@@ -1,7 +1,18 @@
 function booleanFunction(isTrue) {
     if (isTrue) {
-        return "yes is true";
+        return "Yes is true";
     }
-    return "is false";
+    return "No is false";
 }
-booleanFunction(true);
+// isTrue(true);
+console.log(booleanFunction(true));
+console.log(booleanFunction(false));
+
+
+function boolean_function(trueOrFalse) {
+    if (isTrue) {
+        return "is true";
+    }
+    return "false";
+}
+console.log(booleanFunction(true));
